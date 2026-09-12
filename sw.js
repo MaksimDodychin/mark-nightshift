72e67b98b15c42b5e1273adb69f4ae44afa3a40c
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-12 21:21:06 +0300

    Keep newly created objects consistent with night lighting

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = '9c83ed3c29';
+const VERSION = 'fdb1c1356e';
 const PREFIX = "nightshift-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
